8fea4c5b0a31d0e839f134e969dad50668abfdb1
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2020-06-27 14:12:45 -0700

    Initialize canvas

--- src/Canvas/index.js
+++ src/Canvas/index.js
@@ -21,6 +21,39 @@ const useStyles = makeStyles(() => ({
 export default function Canvas(props) {
     const classes = useStyles(props);
     const canvas = React.useRef(null);
+    const [state, setState] = React.useState({
+        gl: null
+    });
+
+    const {gl} = state;
+
+    // Initialize GL context.
+    const onInitiate = React.useCallback(() => {
+        const state = initialize(canvas);
+        setState(prevState => ({...prevState, ...state}));
+
+    }, [canvas]);
+
+    // Render scene.
+    const onRender = React.useCallback(() => {
+        if (!gl)
+            return;
+
+        render(gl);
+
+    }, [gl]);
+
+    // Handle resizing event.
+    React.useEffect(() => {
+        window.addEventListener("resize", onRender);
+        return () => window.removeEventListener("resize", onRender);
+    }, [onRender]);
+
+    // Handle state initialization.
+    React.useEffect(() => onInitiate(), [canvas, onInitiate]);
+
+    // Handle rendering.
+    React.useEffect(() => onRender(), [gl, onRender]);
 
     return (
         <canvas
@@ -30,3 +63,125 @@ export default function Canvas(props) {
         />
     )
 }
+
+
+const initialize = (canvas) => {
+    const params = {
+        alpha: true,
+        depth: false,
+        stencil: false,
+        antialias: false,
+        preserveDrawingBuffer: false
+    };
+
+    let gl = canvas.current.getContext("webgl2", params);
+    const isWebGL2 = !!gl;
+    if (!isWebGL2) {
+        gl = canvas.current.getContext("webgl", params)
+            || canvas.current.getContext("experimental-webgl", params);
+    }
+
+    let halfFloat;
+    let supportLinearFiltering;
+
+    if (isWebGL2) {
+        gl.getExtension("EXT_color_buffer_float");
+        supportLinearFiltering = gl.getExtension("OES_texture_float_linear");
+    }
+    else {
+        halfFloat = gl.getExtension("OES_texture_half_float");
+        supportLinearFiltering = gl.getExtension(
+            "OES_texture_half_float_linear"
+        );
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    const halfFloatTexType = isWebGL2 ?
+        gl.HALF_FLOAT : halfFloat.HALF_FLOAT_OES;
+
+    let formatRGBA;
+    let formatRG;
+    let formatR;
+
+    if (isWebGL2) {
+        formatRGBA = getSupportedFormat(gl, gl.RGBA16F, gl.RGBA, halfFloatTexType);
+        formatRG = getSupportedFormat(gl, gl.RG16F, gl.RG, halfFloatTexType);
+        formatR = getSupportedFormat(gl, gl.R16F, gl.RED, halfFloatTexType);
+    } else {
+        formatRGBA = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType);
+        formatRG = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType);
+        formatR = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType);
+    }
+
+    return {
+        gl: gl,
+        ext: {
+            formatRGBA,
+            formatRG,
+            formatR,
+            halfFloatTexType,
+            supportLinearFiltering
+        }
+    };
+};
+
+
+const getSupportedFormat = (gl, internalFormat, format, type) => {
+    if (!supportRenderTextureFormat(gl, internalFormat, format, type)) {
+        switch (internalFormat) {
+            case gl.R16F:
+                return getSupportedFormat(gl, gl.RG16F, gl.RG, type);
+            case gl.RG16F:
+                return getSupportedFormat(gl, gl.RGBA16F, gl.RGBA, type);
+            default:
+                return null;
+        }
+    }
+
+    return {
+        internalFormat,
+        format
+    }
+};
+
+const supportRenderTextureFormat = (gl, internalFormat, format, type) => {
+    let texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(
+        gl.TEXTURE_2D, 0, internalFormat, 4, 4, 0, format, type, null
+    );
+
+    let fbo = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+    gl.framebufferTexture2D(
+        gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0
+    );
+
+    const status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
+    return status === gl.FRAMEBUFFER_COMPLETE;
+};
+
+
+const render = (gl) => {
+    resize(gl.canvas);
+};
+
+
+const resize = (canvas) => {
+    const dpr = window.devicePixelRatio || 1;
+    const width = Math.floor(canvas.clientWidth * dpr);
+    const height = Math.floor(canvas.clientHeight * dpr);
+
+    if (canvas.width !== width || canvas.height !== height) {
+        canvas.width = width;
+        canvas.height = height;
+    }
+};
+
+
+const isMobile = () => /Mobi|Android/i.test(navigator.userAgent);
