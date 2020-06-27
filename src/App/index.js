779736c92e8b1b58817465217d6c1902d6bfbc7e
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2020-06-27 13:17:06 -0700

    Move theme to high level component

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import {createMuiTheme} from "@material-ui/core/styles";
-import {ThemeProvider} from "@material-ui/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -8,28 +6,12 @@ import Canvas from "../Canvas/index.js";
 import AppDrawer from "../AppDrawer/index.js";
 
 
-const theme = createMuiTheme({
-    palette: {
-        type: "dark",
-        primary: {
-            main: "#212121"
-        },
-        secondary: {
-            main: "#4E342E"
-        }
-    },
-    zIndex: {
-        appBar: 2000
-    }
-});
-
-
 export default function App() {
 
     const drawerWidth = 300;
 
     return (
-        <ThemeProvider theme={theme}>
+        <div>
             <AppBar position="fixed">
                 <Toolbar>
                     <Typography variant="h6" noWrap>
@@ -39,7 +21,7 @@ export default function App() {
             </AppBar>
             <AppDrawer width={drawerWidth} />
             <Canvas paddingLeft={drawerWidth} />
-        </ThemeProvider>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createMuiTheme} from "@material-ui/core/styles";
+import {ThemeProvider} from "@material-ui/styles";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+
+const theme = createMuiTheme({
+    palette: {
+        type: "dark",
+        primary: {
+            main: "#212121"
+        },
+        secondary: {
+            main: "#4E342E"
+        }
+    },
+    zIndex: {
+        appBar: 2000
+    }
+});
+
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </ThemeProvider>,
     document.getElementById("root")
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
